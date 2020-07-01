7f07c33b40bae5892ba91a200754086d8b42f691
Mikhail Budzilovich <58990078+Mibudch@users.noreply.github.com>
2020-07-01 13:25:40 +0300

    2

--- src/toDoList/ToDoList.js
+++ src/toDoList/ToDoList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import InputComponent from './InputComponent'
+import InputComponent from './InputComponent/InputComponent'
 import './ToDoList';
 class ToDoList extends Component {
     constructor(props) {
@@ -8,10 +8,17 @@ class ToDoList extends Component {
             arrTodos: ['task', 'task', 'task', 'task',]
         }
     };
+
     render() {
+        const arrTodos = this.state.arrTodos
         return (
             <>
-                <h1>You have {this.state.arrTodos.length} Todos</h1>
+                <h1>You have {arrTodos.length || 0} Todos</h1>
+                {/* {this.state.arrTodos.map((elem, indx) => {
+                return (
+                    
+                )
+            })} */}
                 <InputComponent />
             </>
         )
