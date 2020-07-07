390794e92ac4cae774dbd7eca7efeb35350fb0e5
Mikhail Budzilovich <58990078+Mibudch@users.noreply.github.com>
2020-07-07 14:14:11 +0300

    1

--- src/MagicBall/BallWrapper.js
+++ src/MagicBall/BallWrapper.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './BallWrapper.css'
 export default props =>(
-    <div style={{display: 'flex', alignContent: 'center', backgroundColor: '#333', width: 100, height: 100}}>
-        <div style={{backgroundColor: 'white', width: 90, height: 90, margin: 'auto'}}>{props.answer}</div>
+    <div className='ball'>
+        <div className='ballCenter'>{props.answer}</div>
     </div>
 )
 
