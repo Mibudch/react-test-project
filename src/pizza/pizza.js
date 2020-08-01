c88a00aef7042182a740d646679e25dea1477f77
Mikhail Budzilovich <58990078+Mibudch@users.noreply.github.com>
2020-08-01 14:37:43 +0300

    Add pizza component

--- src/pizza/pizza.js
+++ src/pizza/pizza.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
 class Pizza extends Component {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            cards: ['Pepperoni', 'Margarita', 'Sorento', 'Italiano']
+        }
+    }
     render() {
-        return ()
+        return (
+        <div>
+            
+        </div>
+        )
     }
 }
 export default Pizza
