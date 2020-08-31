7d1bc954c8d1704fc4009fb2f21606589ebbae49
Mikhail Budzilovich <58990078+Mibudch@users.noreply.github.com>
2020-08-31 14:53:50 +0300

    make a calendar table

--- src/calendar/calendarView/Section.js
+++ src/calendar/calendarView/Section.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './Section.css'
 const weekDays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс',]
 export const Section = (props) => (
-    <section className='flex'>
+    <section className= 'width'>
+        <div className='flex'>
             {weekDays.map((el, i) => {
                 return (
-            <div key={i} className='size border centried'>{el}</div>
+                    <div key={i} className='size border centried'>{el}</div>
                 )
             })}
-            <div>{props.numberOfDays}</div>
+        </div>
+        <div className='flex wrap'>
+            {props.numberOfDays.map((el, i) => {
+                return (
+                    <div key={i} className='size border centried'>{el}</div>
+                )
+            })}
+        </div>
     </section>
 )
